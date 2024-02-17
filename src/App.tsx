a613094efc858faff06269b1b43e6af51378b69e
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-17 20:35:07 -0300

    feature: install tanstack query and heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,4 @@
-import axios from "axios";
-import { useEffect } from "react";
-
 function App() {
-  const url = import.meta.env.VITE_APP_API_URL;
-
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
   return <></>;
 }
 
