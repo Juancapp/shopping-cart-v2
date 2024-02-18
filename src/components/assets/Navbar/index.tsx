4a05354f313d685d284e22bcc19b8e69816620a9
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-18 03:44:35 -0300

    feature: create Select and Navbar component

---
+++ src/components/assets/Navbar/index.tsx
@@ -0,0 +1,27 @@
+// import {
+//   BuildingStorefrontIcon,
+//   ShoppingCartIcon,
+// } from "@heroicons/react/24/outline";
+// import Select from "../Select";
+
+// function Navbar() {
+//   return (
+//     <div>
+//       <div>
+//         <div>
+//           <BuildingStorefrontIcon />
+//           <span>Cocoa</span>
+//         </div>
+//         <ShoppingCartIcon />
+//       </div>
+//       <search></search>
+//       <div>
+//         <Select />
+//         <Select />
+//         <input type="text" />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-bg-color": "#273043",
+      },
+    },
   },
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   plugins: [],
