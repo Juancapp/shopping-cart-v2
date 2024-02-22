3e47ff78d7eb0b6beccb9dcaa26ae601a92431eb
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-22 03:37:41 -0300

    loader

--- src/components/pages/ProductsContainer/index.tsx
+++ src/components/pages/ProductsContainer/index.tsx
@@ -30,36 +30,43 @@ function ProductsContainer() {
     setSearchParams(paramsObj);
   }, [page]);
 
-  if (query.isLoading) return <h1>cargando</h1>;
-
-  if (query.isError) return <h1>Error</h1>;
-
   return (
-    <div className="px-40 py-20 flex flex-col gap-20 items-center">
+    <div className="px-[5%] py-20 flex flex-col gap-20 items-center">
       <h1 className="text-5xl w-fit">Products</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 box-content">
-        {data?.products.map((product) => {
-          return (
-            <div
-              className="hover:shadow-xl cursor-pointer px-10 py-3 flex flex-col items-center"
-              key={product.id}
-            >
-              <img
-                src={product.image}
-                alt={product.title}
-                className="object-contain w-full h-2/3 box-content"
-              />
-              <p className="font-bold text-center">{product.title}</p>
-              <p>Price: {product.price}</p>
-              <p>Rating: {product?.rating?.rate}</p>
-            </div>
-          );
-        })}
-        <div>{data?.totalPages}</div>
-        <div>Page: {page}</div>
+      {query.isFetching ? (
+        <div className="flex space-x-2 animate-pulse">
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 box-content">
+          {data?.products.map((product) => {
+            return (
+              <div
+                className="hover:shadow-xl cursor-pointer px-10 py-3 flex flex-col items-center"
+                key={product.id}
+              >
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="object-contain w-full h-2/3 box-content"
+                />
+                <p className="font-bold text-center">{product.title}</p>
+                <p className="font-bold text-center">${product.price}</p>
+                <p>Rating: {product?.rating?.rate}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="flex flex-row">
         {page > 1 && (
           <button onClick={() => handlePreviousPage()}>Previous</button>
         )}
+        <div>
+          {page} of {data?.totalPages}
+        </div>
         {page < data?.totalPages && (
           <button onClick={() => handleNextPage()}>Next</button>
         )}
