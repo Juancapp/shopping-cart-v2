59c4011aac6dfdff6111c2e62380a9b9a63adea1
Juancapp <juan.cappuccio@radiumrocket.com>
2024-03-08 22:09:16 -0300

    add flex in documentation navbar buttom in mobile version

--- src/components/assets/Navbar/index.tsx
+++ src/components/assets/Navbar/index.tsx
@@ -190,7 +190,7 @@ function Navbar() {
                       item.current
                         ? "bg-gray-900 text-white"
                         : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "block rounded-md px-3 py-2 text-base font-medium"
+                      "rounded-md px-3 py-2 text-base font-medium flex"
                     )}
                     aria-current={item.current ? "page" : undefined}
                   >
